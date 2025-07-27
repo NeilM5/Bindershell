bf9dc71fd0333a4d4cdd4e409c89075592c190c2
NeilM5 <167147911+NeilM5@users.noreply.github.com>
2025-07-27 19:30:16 -0400

    Update Globals.cs

--- Globals.cs
+++ Globals.cs
@@ -2,7 +2,7 @@ using System.Diagnostics;
 public static class Globals
 {
     public static string currentDir = Directory.GetCurrentDirectory();
-    public static string version = "v1.0.0-a";
+    public static string version = "v1.0.0";
 
     public static List<string> commandHistory = new();
 
