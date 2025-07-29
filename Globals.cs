aef0af8b596bb6e5fd38bbcad3cf5bc290553193
NeilM5 <167147911+NeilM5@users.noreply.github.com>
2025-07-28 20:27:46 -0400

    Updated Version
    - v1.2.2

--- Globals.cs
+++ Globals.cs
@@ -2,7 +2,7 @@ using System.Diagnostics;
 public static class Globals
 {
     public static string currentDir = Directory.GetCurrentDirectory();
-    public static string version = "v1.1.2";
+    public static string version = "v1.2.2";
 
     public static List<string> commandHistory = new();
 
